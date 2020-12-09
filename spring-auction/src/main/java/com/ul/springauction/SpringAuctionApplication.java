6a038b821f97a16451aafe1ca694ccfabbb303c6
AnnYoS <simon.yoann54@gmail.com>
2020-12-09 16:44:28 +0100

    initialisation du projet spring

---
+++ spring-auction/src/main/java/com/ul/springauction/SpringAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.ul.springauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuctionApplication.class, args);
+    }
+
+}
---
+++ spring-auction/src/test/java/com/ul/springauction/SpringAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ul.springauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
