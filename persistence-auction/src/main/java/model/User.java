8858d94366c59191bff1a234f48c5df6779c3704
Reynault Sies <reynault.sies@gmail.com>
2021-01-13 16:40:52 +0100

    ajout de la liaison entre l'utilisateur et son adresse

--- persistence-auction/src/main/java/model/User.java
+++ persistence-auction/src/main/java/model/User.java
@@ -23,19 +23,24 @@ public class User {
     private String name;
     private String lastname;
 
-    public User(long id, String login, String pass, String name, String lastname) {
+    @OneToOne(targetEntity = Address.class)
+    private Address home;
+
+    public User(long id, String login, String pass, String name, String lastname, Address home) {
         this.id = id;
         this.login = login;
         this.pass = pass;
         this.name = name;
         this.lastname = lastname;
+        this.home = home;
     }
 
-    public User(String login, String pass, String name, String lastname) {
+    public User(String login, String pass, String name, String lastname, Address home) {
         this.login = login;
         this.pass = pass;
         this.name = name;
         this.lastname = lastname;
+        this.home = home;
     }
 
     public User() {
@@ -80,4 +85,13 @@ public class User {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+
+    public Address getHome() {
+        return home;
+    }
+
+    public void setHome(Address home) {
+        this.home = home;
+    }
+
 }
