645466c6d05332b3d385c5c252573e7c4c64b115
AnnYoS <simon.yoann54@gmail.com>
2021-01-25 17:30:06 +0100

    ajout /auction

--- spring-auction/src/main/java/com/ul/springauction/controller/ParticipationController.java
+++ spring-auction/src/main/java/com/ul/springauction/controller/ParticipationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping(value = "/participation")
+@RequestMapping(value = "/auction/participation")
 @RestController
 @CrossOrigin(origins = "http://localhost:5201")
 public class ParticipationController {
--- spring-auction/src/main/java/com/ul/springauction/controller/PromotionController.java
+++ spring-auction/src/main/java/com/ul/springauction/controller/PromotionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
-@RequestMapping(value = "/participation")
+@RequestMapping(value = "/auction/participation")
 @RestController
 @CrossOrigin(origins = "http://localhost:5201")
 public class PromotionController {
