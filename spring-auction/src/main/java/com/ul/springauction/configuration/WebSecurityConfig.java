07a94441f84911f8c856df359844916a16e08486
AnnYoS <simon.yoann54@gmail.com>
2021-01-27 11:25:19 +0100

    commit

--- spring-auction/src/main/java/com/ul/springauction/configuration/WebSecurityConfig.java
+++ spring-auction/src/main/java/com/ul/springauction/configuration/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ul.springauction.configuration;
 
-import org.apache.tomcat.util.file.ConfigurationSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -37,24 +36,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5201"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
-
-    // Configuration de la sécrutié
+    // Configuration de la sécurité
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
         // Configure Spring Security pour autorisé tout les cors
-        http.cors();
+        http.cors().configurationSource(config -> new CorsConfiguration().applyPermitDefaultValues());
 
         // On ajoute la sécurité de vérifier le token a chaque requête
         http.headers().frameOptions().disable();
