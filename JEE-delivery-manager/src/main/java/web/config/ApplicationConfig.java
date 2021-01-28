6cf7e91c0d4fb23f7bccc07c09fb9f30927f2f11
Reynault Sies <reynault.sies@gmail.com>
2021-01-28 15:02:33 +0100

    ajout cors

--- JEE-delivery-manager/src/main/java/web/config/ApplicationConfig.java
+++ JEE-delivery-manager/src/main/java/web/config/ApplicationConfig.java
@@ -20,6 +20,7 @@ public class ApplicationConfig extends Application {
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(web.controller.DeliveryController.class);
+        resources.add(web.config.cors.CORSFilter.class);
     }
 
 }
